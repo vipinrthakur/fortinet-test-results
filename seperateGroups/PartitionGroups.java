db60994035a8529de6804dc600a451d460c3cf37
Vipin Thakur <vipinrthakur@gmail.com>
2017-09-18 10:11:54 -0700

    Update PartitionGroups.java

--- seperateGroups/PartitionGroups.java
+++ seperateGroups/PartitionGroups.java
@@ -3,14 +3,12 @@ package seperateGroups;
 import java.util.List;
 
 public class PartitionGroups {
-	
 	/**
-	 * method to partition integers into k groups and check if all groups has same sum
-	 * Time Complexity: O(nk) -> n is size of list, k is number of subsets
-	 * Space Complexity: O(k) -> k (k<=n)
+	 * method to partition integers into k groups and check if all groups has
+	 * same sum Time Complexity: O(nk) -> n is size of list, k is number of
+	 * subsets Space Complexity: O(k) -> k (k<=n)
 	 * 
 	 * @author vthakur
-	 * 
 	 * @param list
 	 * @param k
 	 * @return
@@ -20,15 +18,12 @@ public class PartitionGroups {
 			return false;
 		}
 		int size = list.size();
-		
 		if (k == 1) {
 			return true;
 		}
-		
 		if (size < k) {
 			return false;
 		}
-		
 		int sum = 0;
 		for (int i = 0; i < size; i++) {
 			sum += list.get(i);
@@ -36,29 +31,16 @@ public class PartitionGroups {
 		if (sum % k != 0) {
 			return false;
 		}
-		
 		int subset = sum / k;
 		int subsetSum[] = new int[k];
 		boolean taken[] = new boolean[size];
-		
-		for (int i = 0; i < k; i++) {
-			subsetSum[i] = 0;
-		}
-		for (int i = 0; i < size; i++) {
-			taken[i] = false;
-		}
 		subsetSum[0] = list.get(size - 1);
 		taken[size - 1] = true;
-		if (subset < subsetSum[0]) {
-			return false;
-		}
-		
 		return isPartitionPossibleRecursion(list, subsetSum, taken, subset, k, size, 0, size - 1);
 	}
-	
+
 	/**
 	 * recursive helper function
-	 * 
 	 * 
 	 * @param list
 	 * @param subsetSum
@@ -70,7 +52,8 @@ public class PartitionGroups {
 	 * @param limitIndex
 	 * @return
 	 */
-	private static boolean isPartitionPossibleRecursion(List<Integer> list, int[] subsetSum, boolean[] taken, int subset, int k, int size, int currentIndex, int limitIndex) {
+	private static boolean isPartitionPossibleRecursion(List<Integer> list, int[] subsetSum, boolean[] taken, int subset, int k, int size,
+			int currentIndex, int limitIndex) {
 		if (subsetSum[currentIndex] == subset) {
 			if (currentIndex == k - 2) {
 				return true;
@@ -82,7 +65,6 @@ public class PartitionGroups {
 			if (taken[i]) {
 				continue;
 			}
-			
 			int tmp = subsetSum[currentIndex] + list.get(i);
 			if (tmp <= subset) {
 				taken[i] = true;
@@ -90,12 +72,11 @@ public class PartitionGroups {
 				boolean nxt = isPartitionPossibleRecursion(list, subsetSum, taken, subset, k, size, currentIndex, i - 1);
 				taken[i] = false;
 				subsetSum[currentIndex] -= list.get(i);
-				if (nxt)
+				if (nxt) {
 					return true;
+				}
 			}
 		}
-		    return false;
+		return false;
 	}
-
-	
 }
